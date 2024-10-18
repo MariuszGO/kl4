18ab23fed5f2291b3d5ada7ac8e41e7ad9045ee6
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-10-18 11:06:10 +0200

    n11

---
+++ nowy.js
@@ -0,0 +1,16 @@
+
+document.getElementById("a2").innerHTML = "zzz";
+//function suma() {
+  //  document.getElementById("s1").innerHTML = "Suma to 100000";
+ // }
+
+function suma1() {
+    document.getElementById("s2").innerHTML = "Suma to 100000";
+  }
+ 
+
+  function suma2() {
+    document.getElementById("s1").innerHTML = "Suma to 100000";
+  }
+
+  suma2();
