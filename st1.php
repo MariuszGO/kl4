b46054871f815d2f601cdd08407f706589ae0b10
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-11-29 09:56:37 +0100

    p1

---
+++ st1.php
@@ -0,0 +1,40 @@
+<?php
+
+
+//instrukcja if
+$zmienna = true;
+
+if($zmienna){
+    echo "Prawda";
+}
+else{
+    echo "Fałsz";
+}
+
+
+$a = 5;
+$b = 10;
+
+if($a<$b){
+    echo "a mniejsze od b";
+}
+elseif($a>$b){
+    echo "a większe od b";
+}
+else{
+    echo "a jest równe b";
+}
+
+
+//pętla for
+echo "<br><br>";
+for($i=0;$i<100;$i++){
+    echo "$i ";
+}
+
+
+
+
+
+?>
+
---
+++ st2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="st2.php" method="GET">
+        <input type="text" name="liczba">
+        <input type="submit" value="Sprawdź">
+    </form>
+    
+    <?php
+
+
+if(isset($_GET['liczba'])){
+
+$b = $_GET['liczba'];
+
+if( $b > 50 && $b < 150){
+    echo "b: $b jest z przedziału od 50 do 150";
+}
+else {
+    echo "b:  $b jest poza przedziałam";
+}
+
+}
+
+
+?>
+
+
+</body>
+</html>
