e71ce85cb1bb1518597b31807e286210a82d58c6
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-11-29 10:11:16 +0100

    p2

--- st1.php
+++ st1.php
@@ -27,14 +27,40 @@ else{
 
 
 //pętla for
-echo "<br><br>";
+echo "<br>for<br>";
 for($i=0;$i<100;$i++){
     echo "$i ";
 }
 
+//pętla while
+echo "<br>while<br>";
+$i=0;
+while($i<100){
+    echo "$i ";
+    $i++;
+}
+echo "<br>do while<br>";
+$i=0;
+do{
+    echo "$i ";
+    $i++;
+}while($i<100);
 
+echo "<br>foreach<br>";
+$tablica = array("poniedziałek","wtorek","środa","czwartek","piątek","sobota","niedziela");
+$i=0;
+foreach($tablica as $klucz){
+    echo "$klucz <br>";
+}
+echo "<br>";
 
-
+$tab[0]="JAN";
+$tab[1]="ADAM";
+$tab[2]="PAWEŁ";
+$tab[3]="KRZYSZTOF";
+foreach($tab as $klucz){
+    echo "$klucz <br>";
+}
 
 ?>
 
