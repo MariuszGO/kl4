d93147e7df2162408e155d525d088523ab10e084
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-12-02 12:59:06 +0100

    p3

---
+++ pierwszy.php
@@ -0,0 +1,25 @@
+<?php
+
+$zmienna = 15;     
+$zmienna2 = "15";
+$zmienna3 = "ADAM";
+$zmienna4 = 3.555;
+$zmienna5 = true;
+$tab = array (3,35,335,34);
+$t[1]= 3434;
+
+echo "Witaj Świecie";
+echo "<br>";
+var_dump($zmienna);
+echo "<br>";
+echo "<h1>To jest zmienna </h1>";
+var_dump($zmienna3);
+
+
+echo "<br>Zmienna jeden ma wartość $zmienna <br>";
+echo "Zmienna jeden ma wartosć " . $zmienna . " " .$zmienna4;
+
+
+
+
+?>
