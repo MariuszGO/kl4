322be989b5188931312d3d7d188bd68bffef30c3
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2024-12-02 13:27:47 +0100

    p3

--- pierwszy.php
+++ pierwszy.php
@@ -10,6 +10,21 @@ $d = $a + $b;
 //= przypisz
 // == czy są równe
 // === czy są równe i są tego samego typu
+// != różne
+// <= 
+// >=
+
+$e = 20;
+
+
+// większa od 10
+// większa od 20 a mniejsza od 30
+// wieksza od 30 a mniejsza od 40 
+// poza podanymi zakresami
+//if (warunek1 && warunrk2)
+//if (warunek1 and warunek2)
+
+
 
 if ($a === $b){
     echo "równe";
